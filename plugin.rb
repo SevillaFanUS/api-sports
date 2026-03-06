40f25bfaf8396bb02d8c0d484d039cbbaba1f5d8
SevillaFanUS <chris@chrislail.net>
2026-03-05 19:57:40 -0500

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -7,25 +7,3 @@
 # url: https://monchismen.com
 
 enabled_site_setting :api_sports_enabled
-
-after_initialize do
-  WIDGET_TAG = "api-sports-widget"
-
-  WIDGET_ATTRIBUTES = %w[
-    data-type data-sport data-key data-id data-season data-date data-teams
-    data-theme data-lang data-refresh data-show-logos data-show-error
-    data-favorite data-standings data-player-trophies data-player-injuries
-    data-team-squad data-team-statistics data-player-statistics data-tab
-    data-game-tab data-target-player data-target-league data-target-team
-    data-target-game data-target-standings
-  ].freeze
-
-  # Discourse uses PrettyText which exposes an allowlist API for plugins.
-  # This is the correct, non-frozen-safe way to extend the sanitizer.
-  if defined?(PrettyText)
-    PrettyText.add_allowlist(
-      elements: [WIDGET_TAG],
-      attributes: { WIDGET_TAG => WIDGET_ATTRIBUTES }
-    )
-  end
-end
