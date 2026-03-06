7f20f2fe6614d1834fe43f8045a9772265e6a310
SevillaFanUS <chris@chrislail.net>
2026-03-05 20:30:58 -0500

    Add files via upload

---
+++ plugin.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+# name: discourse-api-sports
+# about: Embeds api-sports.io football widgets into Discourse posts via a composer toolbar button
+# version: 1.0.0
+# authors: MonchisMen
+# url: https://monchismen.com
+
+enabled_site_setting :api_sports_enabled
