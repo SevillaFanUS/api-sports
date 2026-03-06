cc72650629d3530a977b135aa84b70f65512fef3
SevillaFanUS <chris@chrislail.net>
2026-03-05 19:35:07 -0500

    Update html_allowlist.rb

--- lib/api_sports/html_allowlist.rb
+++ lib/api_sports/html_allowlist.rb
@@ -5,44 +5,42 @@
 
 module ApiSports
   module HtmlAllowlist
-    def self.apply!
-      # Allowlist the custom element tag
-      HtmlSanitize::ALLOWED_ELEMENTS_WITH_OPTIONAL_ATTRIBUTES.push(
-        "api-sports-widget"
-      ) unless HtmlSanitize::ALLOWED_ELEMENTS_WITH_OPTIONAL_ATTRIBUTES.include?("api-sports-widget")
+    WIDGET_ATTRIBUTES = %w[
+      data-type
+      data-sport
+      data-key
+      data-id
+      data-season
+      data-date
+      data-teams
+      data-theme
+      data-lang
+      data-refresh
+      data-show-logos
+      data-show-error
+      data-favorite
+      data-standings
+      data-player-trophies
+      data-player-injuries
+      data-team-squad
+      data-team-statistics
+      data-player-statistics
+      data-tab
+      data-game-tab
+      data-target-player
+      data-target-league
+      data-target-team
+      data-target-game
+      data-target-standings
+    ].freeze
 
-      # Allowlist all data-* attributes on the element
-      %w[
-        data-type
-        data-sport
-        data-key
-        data-id
-        data-season
-        data-date
-        data-teams
-        data-theme
-        data-lang
-        data-refresh
-        data-show-logos
-        data-show-error
-        data-favorite
-        data-standings
-        data-player-trophies
-        data-player-injuries
-        data-team-squad
-        data-team-statistics
-        data-player-statistics
-        data-tab
-        data-game-tab
-        data-target-player
-        data-target-league
-        data-target-team
-        data-target-game
-        data-target-standings
-      ].each do |attr|
-        unless HtmlSanitize::ALLOWED_ATTRIBUTES.include?(attr)
-          HtmlSanitize::ALLOWED_ATTRIBUTES.push(attr)
-        end
+    def self.apply!
+      Loofah::HTML5::SafeListProtocol::ALLOWED_ELEMENTS_WITH_OPTIONAL_ATTRIBUTES.add(
+        "api-sports-widget"
+      )
+
+      WIDGET_ATTRIBUTES.each do |attr|
+        Loofah::HTML5::SafeListProtocol::ALLOWED_ATTRIBUTES.add(attr)
       end
     end
   end
