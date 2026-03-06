4111f4003a4c25c937193e24d890d4bf16c7ab1d
SevillaFanUS <chris@chrislail.net>
2026-03-06 11:01:17 -0500

    Delete plugin.rb

--- plugin.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# name: discourse-api-sports
-# about: Embeds api-sports.io football widgets into Discourse posts via a composer toolbar button
-# version: 1.0.0
-# authors: MonchisMen
-# url: https://monchismen.com
-
-enabled_site_setting :api_sports_enabled
